97915ffef8a635e9f2e720c3b2ec898b8eaed6ab
siang <siang@alumni.ubc.ca>
2015-11-06 21:45:24 -0800

    'initial'

---
+++ js/custom.js
@@ -0,0 +1,73 @@
+/*
+  * @package Cyprass
+  * @subpackage Cyprass HTML
+  * 
+  * Template Scripts
+  * Created by Tripples
+  
+  1. Fixed header
+  2. Site search
+  3. Main slideshow
+  4. Owl Carousel
+      a. Testimonial
+      b. Clients
+      c. Team
+  5. Back to top
+  6. Skills
+  7. BX slider
+      a. Blog Slider
+      b. Portfolio item slider
+  8. Isotope
+  9. Animation (wow)
+  10. Flickr
+  
+*/
+
+
+jQuery(function($) {
+  "use strict";
+
+
+    $.noConflict();
+     $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+
+
+  /* ----------------------------------------------------------- */
+  /*  Main slideshow
+  /* ----------------------------------------------------------- */
+
+  $('#slider_part').carousel({
+    pause: true,
+    interval: 100000,
+  });
+
+/* ==============================================
+Back To Top Button
+=============================================== */  
+ 
+  $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+      // scroll body to 0px on click
+      $('#back-top').click(function () {
+          $('#back-top a').tooltip('hide');
+          $('body,html').animate({
+              scrollTop: 0
+          }, 800);
+          return false;
+      });
+      
+      $('#back-top').tooltip('hide');
+
+
+
+
+});
